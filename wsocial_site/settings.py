87ba14f542553004a2748027e623e536047ed0a0
Dustin King <cathodion@gmail.com>
2022-04-20 16:51:39 -0400

    settings update

--- wsocial_site/settings.py
+++ wsocial_site/settings.py
@@ -56,7 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'websocial.apps.WebsocialConfig',
+    #'websocial.apps.WebsocialConfig',
+    'websocial',
 ]
 
 MIDDLEWARE = [
